ecb2ee159bfe23b74406164c5d5a05f503b455ab
CHUSUEYEON <rmffkfk226@naver.com>
2025-04-14 01:27:25 +0900

    ✨feat-letters : 편지 생성 기능

--- src/letters/letters.controller.ts
+++ src/letters/letters.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { LettersService } from './letters.service';
+import { Letter } from './letters.model';
 
 @Controller('letters')
 export class LettersController {
   constructor(private lettersService: LettersService) {}
 
   @Get()
-  getAllLetters() {
+  getAllLetters(): Letter[] {
     console.log(this.lettersService.getAllLetters());
     return this.lettersService.getAllLetters();
+  }
+
+  @Post()
+  createLetter(@Body('letterContent') letterContent: string): Letter {
+    console.log(letterContent);
+    return this.lettersService.createLetter(letterContent);
   }
 }
--- src/letters/letters.service.ts
+++ src/letters/letters.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Letter, letterScope } from './letters.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class LettersService {
-  private letters = [];
+  private letters: Letter[] = [];
 
-  getAllLetters() {
+  getAllLetters(): Letter[] {
     return this.letters;
+  }
+
+  createLetter(letterContent: string): Letter {
+    const letter: Letter = {
+      letterIdx: uuid(), // 실제론 DB에서 생성되거나 UUID 등으로 대체
+      letterContent,
+      letterColor: 1,
+      letterShape: 1, // 기본 값 설정
+      letterFile: 0, // 파일 없음
+      letterIsReported: false, // 기본값 신고 X
+      letterScope: letterScope.GLOBAL, // 기본 공개범위
+      letterIsPublic: true, // 전체 공개 기본
+      letterCreatedAt: new Date().toISOString(),
+      letterDeletedAt: null, // 아직 삭제 안 됨
+    };
+
+    this.letters.push(letter);
+    return letter;
   }
 }
