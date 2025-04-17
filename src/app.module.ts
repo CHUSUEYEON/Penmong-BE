0215efbbdaf283532509a85e9fb72aba29e3c8df
CHUSUEYEON <rmffkfk226@naver.com>
2025-04-18 02:20:49 +0900

    🔧chore: DB 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LettersModule } from './letters/letters.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [LettersModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), LettersModule],
 })
 export class AppModule {}
