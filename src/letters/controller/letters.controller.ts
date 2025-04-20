52d766b6fca0b796e751e4519605b59b2b19f12c
CHUSUEYEON <rmffkfk226@naver.com>
2025-04-20 21:36:09 +0900

    ✨feat-swagger: Letter api에 스웨거 적용

---
+++ src/letters/controller/letters.controller.ts
@@ -0,0 +1,80 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { LettersService } from '../service/letters.service';
+import { CreateLetterDto } from '../dto/create-letter.dto';
+import { Letter } from '../model/letters.entity';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+
+@ApiTags('Letter') // 스웨거에 태그 생성
+@Controller('letters')
+export class LettersController {
+  constructor(private lettersService: LettersService) {}
+
+  @ApiOperation({
+    summary: '전체 편지를 조회하는 api입니다.',
+  })
+  @Get()
+  getAllLetters(): Promise<Letter[]> {
+    console.log(this.lettersService.getAllLetters());
+    return this.lettersService.getAllLetters();
+  }
+
+  @ApiOperation({
+    summary: '상세 편지 하나를 조회하는 api입니다.',
+  })
+  @ApiParam({
+    name: 'letterId',
+    type: 'number',
+    description: '조회할 편지의 id 입력',
+  })
+  @Get('/:letterId')
+  getByLetterId(
+    @Param('letterId', ParseIntPipe) letterId: number,
+  ): Promise<Letter> {
+    return this.lettersService.getLetterById(letterId);
+  }
+
+  @ApiOperation({
+    summary: '편지를 생성하는 api입니다.',
+  })
+  @ApiBody({
+    description: '편지 생성 body',
+    type: CreateLetterDto,
+  })
+  @Post()
+  @UsePipes(ValidationPipe)
+  createLetter(@Body() createLetterDto: CreateLetterDto): Promise<Letter> {
+    console.log(createLetterDto);
+    return this.lettersService.createLetter(createLetterDto);
+  }
+
+  @ApiOperation({
+    summary: '편지를 삭제하는 api입니다.',
+  })
+  @ApiParam({
+    name: 'letterId',
+    type: 'number',
+    description: '삭제할 편지의 id 입력',
+  })
+  @Delete('/:letterId')
+  async deleteLetter(
+    @Param('letterId', ParseIntPipe) letterId: number,
+  ): Promise<void> {
+    await this.lettersService.deleteLetter(letterId);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swaggerConfig } from './configs/swagger.config';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new swaggerConfig().initializeOptions();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-api', app, document); // 스웨거 페이지 경로 설정
+
   await app.listen(3000);
 }
 bootstrap();
