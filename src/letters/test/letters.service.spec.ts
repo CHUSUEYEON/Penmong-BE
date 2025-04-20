b29b2099c18b9571c366a6e05055837b9614ec1a
CHUSUEYEON <rmffkfk226@naver.com>
2025-04-20 19:28:17 +0900

    🐛fix-swagger: 경로 오류로 인한 버그 수정

--- src/letters/test/letters.service.spec.ts
+++ src/letters/test/letters.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { LettersService } from './letters.service';
+import { LettersService } from '../service/letters.service';
 
 describe('LettersService', () => {
   let service: LettersService;
