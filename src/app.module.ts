1cc3fc57bb3cfb13cd5e62671a7acb6b95410e74
CHUSUEYEON <rmffkfk226@naver.com>
2025-04-20 20:27:55 +0900

    ✨feat-env: 환경 변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { LettersModule } from './letters/letters.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), LettersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 모든 모듈에서 환경 변수 접근 가능
+    }),
+    TypeOrmModule.forRoot(typeORMConfig),
+    LettersModule,
+  ],
 })
 export class AppModule {}
