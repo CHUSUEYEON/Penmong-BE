0b13b761bd1ec7afb18fd78ede6a6f840ddb7456
CHUSUEYEON <rmffkfk226@naver.com>
2025-04-26 23:33:39 +0900

    ✨feat-user: 인증인가 기능 스웨거 작업

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,11 +1,20 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from 'src/auth/dto/authCredentials.dto';
 import { AuthService } from '../service/auth.service';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: '로그인 API입니다.',
+  })
+  @ApiBody({
+    description: '로그인 시 필요한 데이터',
+    type: AuthCredentialsDto,
+  })
   @Post('/login')
   signin(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -7,7 +7,7 @@ import {
 } from '@nestjs/common';
 import { UserService } from '../service/user.service';
 import { User } from '../model/user.entity';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 
@@ -16,6 +16,13 @@ import { AuthGuard } from '@nestjs/passport';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({
+    summary: '회원가입 API입니다.',
+  })
+  @ApiBody({
+    description: '회원가입 시 필요한 데이터',
+    type: CreateUserDto,
+  })
   @Post()
   signup(@Body(ValidationPipe) createUserDto: CreateUserDto): Promise<User> {
     return this.userService.signup(createUserDto);
